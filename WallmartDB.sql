4f407665b1cc8a17ecf53584e0567515743dfa81
daveyJones49 <davidmckinney914@gmail.com>
2018-04-06 21:54:10 -0500

    ALL: Added constraints

--- WallmartDB.sql
+++ WallmartDB.sql
@@ -25,30 +25,31 @@ CREATE TABLE EMPLOYEE (
     date_of_hire                DATE            NOT NULL,
     shift_number                INTEGER                 ,
     pto_amount                  INTEGER                 ,
-    fk_superior_id              INTEGER         NOT NULL,
-    fk_store_id			        INTEGER		    NOT NULL,
+    fk_superior_id              INTEGER         		,
+    fk_store_id			        INTEGER		    		,
     PRIMARY KEY (employee_id),
-    FOREIGN KEY (fk_superior_id) REFERENCES EMPLOYEE (employee_id),
+    FOREIGN KEY (fk_superior_id) REFERENCES EMPLOYEE (employee_id)
+		ON DELETE SET NULL 	ON UPDATE CASCADE,
     FOREIGN KEY (fk_store_id) REFERENCES STORE (store_id)
+		ON DELETE SET NULL 	ON UPDATE CASCADE
 );
 
 DROP TABLE IF EXISTS CUSTOMER;
 CREATE TABLE CUSTOMER (
     reward_card_number                  INTEGER         NOT NULL,
-    fk_employee_id                      INTEGER         NOT NULL, 
+    fk_employee_id                      INTEGER         		, 
     name                                VARCHAR(50)     NOT NULL,
     phone_number                        VARCHAR(10)             ,
     PRIMARY KEY (reward_card_number),
-    UNIQUE (fk_employee_id),
-    UNIQUE (reward_card_number),
     FOREIGN KEY (fk_employee_id) REFERENCES EMPLOYEE (employee_id)
+		ON DELETE SET NULL 	ON UPDATE CASCADE
 );
 
 DROP TABLE IF EXISTS DEPARTMENT;
 CREATE TABLE DEPARTMENT (
     department_id               INTEGER         NOT NULL, 
-    fk_manager_id               INTEGER         NOT NULL,
-    fk_store_id                 INTEGER         NOT NULL,
+    fk_manager_id               INTEGER         		,
+    fk_store_id                 INTEGER         		,
     active_registers            INTEGER                 ,
     craft_station_open          BOOLEAN                 ,
     photo_lab_open				BOOLEAN					,
@@ -59,43 +60,41 @@ CREATE TABLE DEPARTMENT (
     department_type             VARCHAR(20)             ,
     total_registers             INTEGER                 ,
     PRIMARY KEY (department_id),
-    FOREIGN KEY (fk_manager_id) REFERENCES EMPLOYEE (employee_id),
+    FOREIGN KEY (fk_manager_id) REFERENCES EMPLOYEE (employee_id)
+		ON DELETE SET NULL 	ON UPDATE CASCADE,
     FOREIGN KEY (fk_store_id)   REFERENCES STORE (store_id)
+		ON DELETE SET NULL 	ON UPDATE CASCADE
 );
 
 DROP TABLE IF EXISTS ENTRY;
 CREATE TABLE ENTRY (
-    fk_employee_id              INTEGER         NOT NULL, 
+    fk_employee_id              INTEGER         		, 
     hours_worked                INTEGER         NOT NULL, 
     hourly_pay                  INTEGER         NOT NULL, 
     PRIMARY KEY (fk_employee_id),
     FOREIGN KEY (fk_employee_id) REFERENCES EMPLOYEE (employee_id)
+		ON DELETE CASCADE 	ON UPDATE CASCADE
 );
 
 DROP TABLE IF EXISTS MANAGER;
 CREATE TABLE MANAGER (
-    fk_employee_id              INTEGER         NOT NULL, 
+    fk_employee_id              INTEGER         		, 
     bonus                       INTEGER                 , 
     salary                      INTEGER         NOT NULL,
     PRIMARY KEY (fk_employee_id),
     FOREIGN KEY (fk_employee_id) REFERENCES EMPLOYEE (employee_id)
-);
-
-DROP TABLE IF EXISTS RESPONSIBILITY;
-CREATE TABLE RESPONSIBILITY (
-    employee_id			        INTEGER		    NOT NULL,
-    responsibility		        VARCHAR(100)	NOT NULL,
-    PRIMARY KEY (employee_id, responsibility),
-    FOREIGN KEY (employee_id) REFERENCES EMPLOYEE (employee_id)
+		ON DELETE CASCADE 	ON UPDATE CASCADE
 );
 
 DROP TABLE IF EXISTS WORKS_IN;
 CREATE TABLE WORKS_IN (
-    fk_department_id            INTEGER         NOT NULL,
-    fk_employee_id              INTEGER         NOT NULL,
+    fk_department_id            INTEGER			NOT NULL,
+    fk_employee_id              INTEGER			NOT NULL,
     PRIMARY KEY (fk_department_id, fk_employee_id),
-    FOREIGN KEY (fk_department_id) REFERENCES DEPARTMENT (department_id),
-    FOREIGN KEY (fk_employee_id) REFERENCES EMPLOYEE (employee_id)
+	FOREIGN KEY (fk_department_id) REFERENCES DEPARTMENT (department_id)
+        ON DELETE CASCADE	ON UPDATE CASCADE,
+	FOREIGN KEY (fk_employee_id) REFERENCES EMPLOYEE (employee_id)
+        ON DELETE CASCADE 	ON UPDATE CASCADE
 );
 
 DROP TABLE IF EXISTS ITEM;
@@ -106,9 +105,12 @@ CREATE TABLE ITEM (
     discount                    INTEGER             	, 
     quantity_in_stock           INTEGER         NOT NULL,
     transaction_id		        INTEGER		    NOT NULL,
-    fk_reward_card_number	    INTEGER		    NOT NULL,
-    fk_department_id		    INTEGER		    NOT NULL,
-    FOREIGN KEY (fk_reward_card_number) REFERENCES CUSTOMER (reward_card_number),
-    FOREIGN KEY (fk_department_id) REFERENCES DEPARTMENT (department_id),
-    PRIMARY KEY (item_id)
+    fk_reward_card_number	    INTEGER		    		,
+    fk_department_id		    INTEGER		    		,
+    PRIMARY KEY (item_id),
+    FOREIGN KEY (fk_reward_card_number) REFERENCES CUSTOMER (reward_card_number)
+		ON DELETE SET NULL 	ON UPDATE CASCADE,
+    FOREIGN KEY (fk_department_id) REFERENCES DEPARTMENT (department_id)
+		ON DELETE SET NULL 	ON UPDATE CASCADE
+    
 );
